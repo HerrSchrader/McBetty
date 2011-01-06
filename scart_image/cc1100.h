71b602220ece0efe0aef6cbbe3d197b5d312d597
Horst Raap <raap@ewetel.net>
2011-01-06 10:05:11 +0100

    default device address is now back to 0x01

--- scart_image/cc1100.h
+++ scart_image/cc1100.h
@@ -111,7 +111,7 @@
 
 
 /* Our own device address */
-#define DEV_ADDR	0x66
+#define DEV_ADDR	0x01
 
 void cc1100_init(void);
 unsigned char cc1100_write(unsigned char addr, unsigned char* dat, unsigned char length);
